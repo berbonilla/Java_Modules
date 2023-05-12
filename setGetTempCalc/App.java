674a4b3ade9c26d8fa92c288dc0f869236a7de1a
Iber Joseph Bonilla <111488477+berbonilla@users.noreply.github.com>
2023-05-12 17:36:31 +0800

    Create App.java

---
+++ setGetTempCalc/App.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class App {
+    public static Scanner sc = new Scanner(System.in);
+    public static char inputM;
+    public static void main(String[] args) {
+    while (inputM!='s'){
+        anotherApp.inputTemp();
+        anotherApp.showTemp();
+    }
+    }
+
+}
+
